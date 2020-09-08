86cbdf8724f889a3b2721f8e9a91758c1d4b24e8
Eugenio Moggio <eugeniom@playtika.com>
2020-09-08 21:18:26 +0100

    [+] first project load

---
+++ Assets/_Eugenio/Scripts/HUDManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour
+{
+    public Image TankIcon;
+
+    public void OnEnable() 
+    {
+        SetColor.onButtonClick += UpdateColor;
+    }
+
+    public void UpdateColor(Color newColor)
+    {
+        TankIcon.color = newColor;
+    }
+
+    public void OnDisable()
+    {
+        SetColor.onButtonClick -= UpdateColor;
+    }
+}
---
+++ Assets/_Eugenio/Scripts/SetColor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using Random=UnityEngine.Random;
+
+public class SetColor : MonoBehaviour
+{
+    //set color sprite
+    public static Action<Color> onButtonClick;
+    private Color _newColor;
+    private Image _currentImage;
+
+    //setColorMaterial
+    //public static Action<Material> onClick;
+
+    void Start()
+    {
+        _currentImage = this.GetComponent<Image>();
+        _newColor = _currentImage.color;
+    }
+
+    public void OnPress()
+    {
+        if(_currentImage==null)
+            _newColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+
+        if(onButtonClick!= null)
+            onButtonClick(_newColor);
+    }
+}
