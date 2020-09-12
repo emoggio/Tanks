f574d61cf0c400874dc050715e4d3d698a28462d
Eugenio Moggio <eugeniom@playtika.com>
2020-09-12 22:48:17 +0100

    [+] added button texture

--- Assets/_Eugenio/Scripts/HUDManager.cs
+++ Assets/_Eugenio/Scripts/HUDManager.cs
@@ -16,25 +16,32 @@ namespace Complete
         public GameObject TankButton;
         public SetColor SetColorScript;
         public GameObject Parent;
-        private Image TankImageFG;
 
         //buttons
-        public Button ButtonSetting;
+        public GameObject Setting;
+        private Button _buttonSetting;
+        private Image _imageSetting, _cogImage;
+        private GameObject _cog;
         public GameObject ButtonColor;
-        public GameObject ButtonPause;
-        public GameObject ButtonQuit;
+        public GameObject ButtonXXX;
+        public GameObject ButtonYYY;
 
         //scrollview
         public GameObject SwatchesScroll;
+        private CanvasGroup _swatchesCanvases;
 
-        private float time = .6f;
-
-        private CanvasGroup SwatchesCanvas;
+        //animations
+        private float _time = .6f;
+        Color _grey = new Color(.4f,.4f,.4f,1);
 
         private void Awake() 
         {
             InstatiateButtons();
-            SwatchesCanvas = SwatchesScroll.GetComponent<CanvasGroup>();
+            _swatchesCanvases = SwatchesScroll.GetComponent<CanvasGroup>();
+            _buttonSetting = Setting.GetComponent<Button>();
+            _imageSetting = Setting.GetComponent<Image>();
+            _cog = Setting.gameObject.transform.GetChild(0).gameObject;
+            _cogImage = _cog.GetComponent<Image>();
         }
 
         public void OnEnable() 
@@ -56,34 +63,45 @@ namespace Complete
         {
             SetColor.onButtonClick -= UpdateColor;
         }
-
+        
         public void ShowButtonMenu()
         {
-            ButtonSetting.interactable = false;
+            RaycastSwitcher();
 
             Sequence HideShow = DOTween.Sequence();
-            HideShow.Append(ButtonColor.transform.DOMoveY(115, time)).SetEase(Ease.OutBack)
-                .Append(ButtonPause.transform.DOMoveY(115, time)).SetEase(Ease.OutBack)
-                .Append(ButtonQuit.transform.DOMoveY(115, time)).SetEase(Ease.OutBack);
+            HideShow.Append(ButtonColor.transform.DOMoveY(115, _time)).SetEase(Ease.OutBack)
+                .Append(ButtonXXX.transform.DOMoveY(115, _time)).SetEase(Ease.OutBack)
+                .Append(ButtonYYY.transform.DOMoveY(115, _time)).SetEase(Ease.OutBack);
         }
 
         public void ShowSwatches()
         {
             Sequence HideShow = DOTween.Sequence();
-            HideShow.Append(ButtonColor.transform.DOMoveY(-300, time/2)).SetEase(Ease.InBack)
-                .Append(ButtonPause.transform.DOMoveY(-300, time/2)).SetEase(Ease.InBack)
-                .Append(ButtonQuit.transform.DOMoveY(-300, time/2)).SetEase(Ease.InBack)
-                .Append(SwatchesScroll.transform.DOMoveY(20, time)).SetEase(Ease.InBack);
+            HideShow.Append(ButtonColor.transform.DOMoveY(-300, _time/2)).SetEase(Ease.InBack)
+                .Append(ButtonXXX.transform.DOMoveY(-300, _time/2)).SetEase(Ease.InBack)
+                .Append(ButtonYYY.transform.DOMoveY(-300, _time/2)).SetEase(Ease.InBack)
+                .Append(SwatchesScroll.transform.DOMoveY(20, _time)).SetEase(Ease.InBack);
         }
 
         public void HideSwatches()
         {
-            SwatchesScroll.transform.DOMoveY(-500, time).SetEase(Ease.InBack).OnComplete(RaycastSwitcher);
+            SwatchesScroll.transform.DOMoveY(-500, _time).SetEase(Ease.InBack).OnComplete(RaycastSwitcher);
         }
-
+        
         void RaycastSwitcher()
         {
-            ButtonSetting.interactable = true;
+            if(_buttonSetting.interactable == false)
+            {
+                _buttonSetting.interactable = true;
+                _imageSetting.color = Color.white;
+                _cogImage.color = Color.white;
+            }
+            else if(_buttonSetting.interactable == true)
+            {
+                _buttonSetting.interactable = false;
+                _imageSetting.color = _grey;
+                _cogImage.color = _grey;
+            }
         }
 
         private void InstatiateButtons()
